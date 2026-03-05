9b8d5c040dd93a34a072c8003fb20bd775c3e83f
camposeddie <eddie.campos@uah.edu>
2026-03-05 13:28:59 -0500

    Update setup.py to latest version

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 import os
-from codecs import open
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open('./requirements.txt', encoding='utf-8') as f:
     reqs = f.read().split('\n')
 install_requires = [x.strip() for x in reqs]
 
 setup(
     name='cumulus-api',
-    version='3.1.0',
+    version='3.2.0',
     author='Abdelhak Marouane',
     description='Python package using Cumulus API which allows developers to interact with the Cumulus Framework,'
                 ' such as monitoring status or creating, editing, and deleting records ',
-    url='https://gitlab.com/ghrc-cloud/cumulus-api',
+    url='https://github.com/nasa/Cumulus-API-Python',
     license='Apache 2.0',
     classifiers=[
         'Framework :: Pytest',
